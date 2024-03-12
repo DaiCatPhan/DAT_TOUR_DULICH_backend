184fbff0e9b0842af65f9d9fffced7a9f333ecef
Phan Dai Cat <phandaicat12032002@gmail.com>
2024-03-12 21:20:32 +0700

    api create booking

--- src/routes/Booking.js
+++ src/routes/Booking.js
@@ -2,6 +2,6 @@ import express from "express";
 const router = express.Router();
 import BookingController from "../controllers/BookingController";
 
-router.post("/", BookingController.booking);
+router.post("/create", BookingController.create);
 
 export default router;
--- src/services/BookingService.js
+++ src/services/BookingService.js
@@ -34,4 +34,94 @@ const remainingSeats = async (idCalendar) => {
   return (+calendarDetail?.numberSeat || 0) - countBookingByCalendar || 0;
 };
 
-export default { countBookingTourByIdCalendar, remainingSeats };
+// Hàm tính tổng tiền
+const prepareTheBill = async (a, b, priceA, priceB) => {
+  return a * priceA + b * priceB;
+};
+
+const createBooking = async (rawData) => {
+  const {
+    ID_Calendar,
+    ID_Customer,
+    ID_Voucher,
+    numberTicketAdult,
+    numberTicketChild,
+    total_money,
+    remaining_money,
+    payment_status,
+    status,
+    cancel_booking,
+    date_cancel_booking,
+    reason_cancel_booking,
+  } = rawData;
+
+  try {
+    const Calendar = await db.Calendar.findByPk(ID_Calendar);
+    const Customer = await db.Calendar.findByPk(ID_Customer);
+
+    if (!Customer) {
+      return {
+        EM: "Khách hàng không tồn tại",
+        EC: -2,
+        DT: [],
+      };
+    }
+
+    if (!Calendar) {
+      return {
+        EM: "Lịch tour không tồn tại",
+        EC: -2,
+        DT: [],
+      };
+    }
+
+    // Kiểm tra chỗ ngồi
+    let tongveCon = await remainingSeats(ID_Calendar);
+    let tongVeDat = numberTicketAdult + numberTicketChild;
+
+    if (tongVeDat > tongveCon) {
+      return {
+        EM: "Hết chỗ !!!",
+        EC: 2,
+        DT: [],
+      };
+    }
+
+    const condition = {};
+    if (ID_Calendar) {
+      condition.ID_Calendar = ID_Calendar;
+    }
+    if (ID_Customer) {
+      condition.ID_Customer = ID_Customer;
+    }
+    if (ID_Voucher) {
+      condition.ID_Voucher = ID_Voucher;
+    }
+    if (numberTicketAdult) {
+      condition.numberTicketAdult = numberTicketAdult;
+    }
+    if (numberTicketChild) {
+      condition.numberTicketChild = numberTicketChild;
+    }
+    if (payment_status) {
+      condition.payment_status = payment_status;
+    }
+
+    const data = await db.BookingTour.create(condition);
+
+    return {
+      EM: "Đặt tour thành công ",
+      EC: 0,
+      DT: data,
+    };
+  } catch (error) {
+    console.log(">>> error", error);
+    return {
+      EM: "Loi server !!!",
+      EC: -5,
+      DT: [],
+    };
+  }
+};
+
+export default { createBooking };
